6b484b3b92f402508bb999055722d359ee4af81c
peterturo <pst17@georgetown.edu>
2022-12-02 17:42:16 -0500

    Minor edits

--- app/bestbets.py
+++ app/bestbets.py
@@ -89,27 +89,23 @@ def generate_arbitrage(SPORT_KEY = "upcoming"):
 
         
 
-        try:
+        best_away_book = (max(a_spreads_dict, key=a_spreads_dict.get))
+        best_home_book = (max(h_spreads_dict, key=h_spreads_dict.get))
+        # from: https://stackoverflow.com/questions/3282823/get-the-key-corresponding-to-the-minimum-value-within-a-dictionary
+            
 
-            best_away_book = (max(a_spreads_dict, key=a_spreads_dict.get))
-            best_home_book = (max(h_spreads_dict, key=h_spreads_dict.get))
-            # from: https://stackoverflow.com/questions/3282823/get-the-key-corresponding-to-the-minimum-value-within-a-dictionary
+            
+        print(f"{sport_title}: {away_team} @ {home_team}")
+        print("")
+
+        print("BEST BETS:")
         
-
-        
-            print(f"{sport_title}: {away_team} @ {home_team}")
-            print("")
-
-            print("BEST BETS:")
-       
-            print(f"{away_team}: {best_away_book} ({plus_sign(a_spreads_dict[best_away_book])}, {plus_sign(a_prices_dict[best_away_book])})")
-            print(f"{home_team}: {best_home_book} ({plus_sign(h_spreads_dict[best_home_book])}, {plus_sign(h_prices_dict[best_home_book])})")
+        print(f"{away_team}: {best_away_book} ({plus_sign(a_spreads_dict[best_away_book])}, {plus_sign(a_prices_dict[best_away_book])})")
+        print(f"{home_team}: {best_home_book} ({plus_sign(h_spreads_dict[best_home_book])}, {plus_sign(h_prices_dict[best_home_book])})")
 
 
-            print("-----------")
+        print("-----------")
 
-        except:
-            break
 
     
 generate_arbitrage()
