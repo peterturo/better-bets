d8663a2da4acba911ebd2e8652448766af409ef8
peterturo <pst17@georgetown.edu>
2022-12-05 21:18:39 -0500

    Running Test

--- test/bestbets_test.py
+++ test/bestbets_test.py
@@ -2,4 +2,6 @@
 from app.bestbets import plus_sign
 
 def test_plus_sign():
-    assert plus_sign()
+    assert plus_sign(-5) == -5
+    assert plus_sign(5) == "+5"
+    assert plus_sign(0) == "PICK"
