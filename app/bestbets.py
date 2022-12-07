f5984b45c21e194d710e008c5ef3a023b4078193
peterturo <pst17@georgetown.edu>
2022-12-07 15:00:01 -0500

    Update

--- app/bestbets.py
+++ app/bestbets.py
@@ -96,10 +96,10 @@ if __name__ == "__main__":
         try:
             best_away_book = (max(a_spreads_dict, key=a_spreads_dict.get))
             best_home_book = (max(h_spreads_dict, key=h_spreads_dict.get))
-            # from: https://stackoverflow.com/questions/3282823/get-the-key-corresponding-to-the-minimum-value-within-a-dictionary
-                
-
-                
+            # from: https://stackoverflow.com/questions/3282823/get-the-key-corresponding-to-the-minimum-value-within-a-dictionary             
+            
+            
+            
             print(f"{sport_title}: {away_team} @ {home_team}")
             print("")
 
