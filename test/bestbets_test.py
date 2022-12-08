0cf4275a39c406292b6f7a9495dae924e1598ccb
peterturo <pst17@georgetown.edu>
2022-12-07 22:17:55 -0500

    Parse Data Testing

--- test/bestbets_test.py
+++ test/bestbets_test.py
@@ -1,5 +1,5 @@
 
-from app.bestbets import plus_sign, Convert
+from app.bestbets import plus_sign, Convert, parse_data
 
 def test_plus_sign():
     assert plus_sign(-5) == -5
@@ -10,3 +10,24 @@ def test_plus_sign():
 def test_Convert():
     list = ['a', 1, 'b', 2, 'c', 3]
     assert Convert(list) == {'a':1, 'b':2, 'c':3}
+
+
+def test_parse_data():
+    
+    games = parse_data(sport_key = "americanfootball_nfl", wager_type = "spreads", book_key = "" )
+    assert isinstance (games, list)
+    
+    for g in games:
+
+        title = g["sport_title"]
+        assert isinstance (title, str)
+        assert title == "NFL"
+
+        away_team = g["away_team"]
+        assert isinstance (away_team, str)
+
+        best_a_spread = g["best_a_spread"]
+        assert isinstance (best_a_spread, float)
+
+
+
