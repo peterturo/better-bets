ff93f11a88474f5940e94804e9b997a18164f7ca
peterturo <111890962+peterturo@users.noreply.github.com>
2022-12-09 18:17:25 -0500

    Tests (#13)
    * More Tests
    
    * Test Edits

--- test/bestbets_test.py
+++ test/bestbets_test.py
@@ -1,5 +1,5 @@
 
-from app.bestbets import plus_sign
+from app.bestbets import plus_sign, fetch_odds, parse_data
 
 def test_plus_sign():
     assert plus_sign(-5) == -5
@@ -7,26 +7,27 @@ def test_plus_sign():
     assert plus_sign(0) == "PICK"
 
 
-#def test_parse_data():
-#    SPORT_KEY = "americanfootball_nfl"
-#    WAGER_TYPE= "spreads"
-#    BOOK_KEY = ""
-#    
-#    games = parse_data(SPORT_KEY, WAGER_TYPE, BOOK_KEY)
-#    
-#    assert isinstance (games, list)
-#    
-#    for g in games:
-#
-#        title = g["sport_title"]
-#        assert isinstance (title, str)
-#        assert title == "NFL"
-#
-#        away_team = g["away_team"]
-#        assert isinstance (away_team, str)
-#
-#        best_a_spread = g["best_a_spread"]
-#        assert isinstance (best_a_spread, float)
+def test_fetch_odds():
+    data = fetch_odds(SPORT_KEY="americanfootball_nfl", WAGER_TYPE="spreads", BOOK_KEY="")
+    assert isinstance (data, list)
+
+
+def test_parse_data():
+
+    games = parse_data(sport_key="americanfootball_nfl", wager_type="spreads", book_key="")
+
+    assert isinstance (games, list)
+
+    for g in games: 
+        title = g["sport_title"]
+        assert isinstance (title, str)
+        assert title == "NFL"   
+        
+        away_team = g["away_team"]
+        assert isinstance (away_team, str)  
+        
+        best_a_spread = g["best_a_spread"][0]
+        assert isinstance (best_a_spread, float)
 
 
 
